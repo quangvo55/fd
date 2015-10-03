b2e7f9092012dff5de6d0a52fe8d94251ff08887
James <Quangvo55@gmail.com>
2015-10-02 17:33:38 -0700

    aded 2015 data

--- optimize.py
+++ optimize.py
@@ -66,7 +66,7 @@ def run():
   solver = pywraplp.Solver('FD', pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING)
 
   all_players = []
-  with open('data/week17.csv', 'rb') as csvfile:
+  with open('data/2015/week3.csv', 'rb') as csvfile:
     csvdata = csv.DictReader(csvfile, skipinitialspace=True)
 
     for row in csvdata:
